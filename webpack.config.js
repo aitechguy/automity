7efaededd3259028743af6ccc440fb314f73d663
Erik Marutian <erik@Eriks-MacBook-Pro.local>
2023-07-19 23:03:23 -0700

    added hot reload - not working

--- webpack.config.js
+++ webpack.config.js
@@ -426,3 +426,40 @@ module.exports = [
   //    Don't actually serve it though when running webpack serve.
   ...(isProd || isWebpackServe ? [] : devClientConfigs),
 ].filter(Boolean);
+
+//hot reload
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  plugins: [isDevelopment && new webpack.HotModuleReplacementPlugin()].filter(Boolean),
+};
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  devServer: {
+    hot: true,
+  },
+};
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [
+                isDevelopment && require.resolve('react-refresh/babel'),
+              ].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [isDevelopment && new ReactRefreshWebpackPlugin()].filter(Boolean),
+};
